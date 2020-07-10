57850356aafa0dc1edf61b0bf0fcc5d3a50c243b
Elizabeth <eh2599@nyu.edu>
2020-07-10 02:45:53 -0500

    Added CSRF protections

--- app.py
+++ app.py
@@ -3,98 +3,92 @@ import subprocess
 
 from flask import Flask, render_template, request, session, make_response
 from flask_bcrypt import Bcrypt
+from flask_wtf.csrf import CSRFProtect
 
 app = Flask(__name__)
+# Set secret key to randomly generated value
 secret_key = secrets.token_urlsafe(32)
 app.config['SECRET_KEY'] = secret_key
 app.config.update(
     SESSION_COOKIE_HTTPONLY=True,
-    SESSION_COOKIE_SAMESITE='Lax',
-    PERMANENT_SESSION_LIFETIME=600
+    SESSION_COOKIE_SAMESITE='Strict',
+    PERMANENT_SESSION_LIFETIME=600,
+    WTF_CSRF_TIME_LIMIT=None
 )
 
+# Reference for bcrypt implementation: https://flask-bcrypt.readthedocs.io/en/latest/
 bcrypt = Bcrypt(app)
 
+# Reference for CSRF implementation: https://flask-wtf.readthedocs.io/en/stable/csrf.html
+csrf = CSRFProtect(app)
+
 registered_users = {}
+
+
+# Reference used for after_request decorator:
+# https://stackoverflow.com/questions/29464276/add-response-headers-to-flask-web-app
+@app.after_request
+def add_security_headers(response):
+    # Reference for Content Securityn Policy implementation: https://csp.withgoogle.com/docs/index.html
+    response.headers['Content-Security-Policy'] = "default-src \'self\' "
+    response.headers['Strict-Transport-Security'] = 'max-age=31536000; includeSubDomains'
+    response.headers['X-Content-Type-Options'] = 'nosniff'
+    return response
 
 
 @app.route('/')
 def index():
     if 'username' in session:
         username = session['username']
-        response = make_response(render_template('logged_in_index.html', username=username))
-        response.headers['Content-Security-Policy'] = "default-src 'self'"
-        return response
+        return render_template('logged_in_index.html', username=username)
     else:
-        response = make_response(render_template('index.html'))
-        response.headers['Content-Security-Policy'] = "default-src 'self'"
-        return response
+        return render_template('index.html')
 
 
 def register_with_user_info(username, hashed_password, phone):
     if username in registered_users:
-        response = make_response(render_template('username_already_exists.html', username=username))
-        response.headers['Content-Security-Policy'] = "default-src 'self'"
-        return response
+        return render_template('username_already_exists.html', username=username)
     else:
         registered_users[username] = [hashed_password, phone]
-        response = make_response(render_template('registration_complete.html', username=username))
-        response.headers['Content-Security-Policy'] = "default-src 'self'"
-        return response
+        return render_template('registration_complete.html', username=username)
 
 
 @app.route('/register', methods=['POST', 'GET'])
 def register():
     if request.method == 'GET':
-        response = make_response(render_template('register.html'))
-        response.headers['Content-Security-Policy'] = "default-src 'self'"
-        return response
+        return render_template('register.html')
     elif request.method == 'POST':
         username = request.values['uname']
         hashed_password = bcrypt.generate_password_hash(request.values['pword']).decode('utf-8')
         phone = request.values['2fa']
-        response = make_response(register_with_user_info(username, hashed_password, phone))
-        response.headers['Content-Security-Policy'] = "default-src 'self'"
-        return response
+        return register_with_user_info(username, hashed_password, phone)
 
 
 def check_user_authentication(username, password, phone):
     if username not in registered_users:
-        response = make_response(render_template('login_failure.html'))
-        response.headers['Content-Security-Policy'] = "default-src 'self'"
-        return response
+        return render_template('login_failure.html')
     else:
         if bcrypt.check_password_hash(registered_users[username][0], password):
             if phone == registered_users[username][1]:
                 session.clear()
                 session['username'] = username
                 session.permanent = True
-                response = make_response(render_template('login_success.html'))
-                response.headers['Content-Security-Policy'] = "default-src 'self'"
-                return response
+                return render_template('login_success.html')
             else:
-                response = make_response(render_template('tfa_failure.html'))
-                response.headers['Content-Security-Policy'] = "default-src 'self'"
-                return response
+                return render_template('tfa_failure.html')
         else:
-            response = make_response(render_template('login_failure.html'))
-            response.headers['Content-Security-Policy'] = "default-src 'self'"
-            return response
+            return render_template('login_failure.html')
 
 
 @app.route('/login', methods=['POST', 'GET'])
 def login():
     if request.method == 'GET':
-        response = make_response(render_template('login.html'))
-        response.headers['Content-Security-Policy'] = "default-src 'self'"
-        return response
+        return render_template('login.html')
     elif request.method == 'POST':
         username = request.values['uname']
         password = request.values['pword']
         phone = request.values['2fa']
-        response = make_response(check_user_authentication(username, password, phone))
-        response.headers['Content-Security-Policy'] = "default-src 'self'"
-        return response
+        return check_user_authentication(username, password, phone)
 
 
 @app.route('/spell_check', methods=['POST', 'GET'])
@@ -102,9 +96,7 @@ def spell_check():
     if 'username' in session:
         username = session['username']
         if request.method == 'GET':
-            response = make_response(render_template('spell_check.html', username=username))
-            response.headers['Content-Security-Policy'] = "default-src 'self'"
-            return response
+            return render_template('spell_check.html', username=username)
         elif request.method == 'POST':
             fp = open('input_text.txt', 'w')
             fp.write(str(request.values['inputtext']))
@@ -112,21 +104,15 @@ def spell_check():
             text_to_check = str(request.values['inputtext'])
             result = subprocess.check_output(["./a.out", "input_text.txt", "wordlist.txt"]).decode(
                 "utf-8").strip().replace('\n', ', ')
-            response = make_response(render_template('spell_check_results.html', text_to_check=text_to_check, result=result))
-            response.headers['Content-Security-Policy'] = "default-src 'self'"
-            return response
+            return render_template('spell_check_results.html', text_to_check=text_to_check, result=result)
     else:
-        response = make_response(render_template('not_logged_in.html'))
-        response.headers['Content-Security-Policy'] = "default-src 'self'"
-        return response
+        return render_template('not_logged_in.html')
 
 
 @app.route('/logout')
 def logout():
     session.pop('username', None)
-    response = make_response(render_template('index.html'))
-    response.headers['Content-Security-Policy'] = "default-src 'self'"
-    return response
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
